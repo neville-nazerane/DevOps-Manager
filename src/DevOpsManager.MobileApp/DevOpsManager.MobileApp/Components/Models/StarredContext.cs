567a5c472092bea7825824b06a8f33b2d121897a
Neville <live.neville@live.com>
2020-02-08 00:56:09 -0700

    starred view

---
+++ src/DevOpsManager.MobileApp/DevOpsManager.MobileApp/Components/Models/StarredContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevOpsManager.MobileApp.Components.Models
+{
+    public class StarredContext
+    {
+
+        public string Identifier { get; set; }
+
+        public int Id { get => int.Parse(Identifier); set => Identifier.ToString(); }
+
+        public bool IsStarred { get; set; }
+
+    }
+}
--- src/DevOpsManager.MobileApp/DevOpsManager.MobileApp/Components/StarredView.cs
+++ src/DevOpsManager.MobileApp/DevOpsManager.MobileApp/Components/StarredView.cs
@@ -1,4 +1,5 @@
-﻿using DevOpsManager.MobileApp.Helpers;
+﻿using DevOpsManager.MobileApp.Components.Models;
+using DevOpsManager.MobileApp.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +10,14 @@ namespace DevOpsManager.MobileApp.Components
     class StarredView : Image
     {
 
-        public static readonly BindableProperty IsStarredProperty = BindableProperty.Create(nameof(IsStarred), typeof(bool), typeof(ScrollView), propertyChanged: OnStarChanged);
-        private bool _isStarred;
+        public static readonly BindableProperty IsStarredProperty = BindableProperty.Create(nameof(IsStarred), typeof(bool), typeof(StarredView), propertyChanged: OnStarChanged);
 
+        public static readonly BindableProperty IdentifierProperty = BindableProperty.Create(nameof(Identifier), typeof(string), typeof(StarredView));
+
+        public static readonly BindableProperty SwitchedCommandProperty = BindableProperty.Create(nameof(SwitchedCommand), typeof(Command<StarredContext>), typeof(StarredView));
+
+        private readonly TapGestureRecognizer _tapGesture;
+        private bool _isStarred;
         public bool IsStarred
         {
             get => _isStarred; 
@@ -20,6 +26,43 @@ namespace DevOpsManager.MobileApp.Components
                 _isStarred = value;
                 if (value) Source = "star_on.png".GetSharedImage();
                 else Source = "star_off.png".GetSharedImage();
+            }
+        }
+
+        public string Identifier { get => (string)GetValue(IdentifierProperty); set => SetValue(IdentifierProperty, value); }
+
+        public Command<StarredContext> SwitchedCommand { get => (Command<StarredContext>)GetValue(SwitchedCommandProperty); set => SetValue(SwitchedCommandProperty, value);   }
+
+
+        public StarredView()
+        {
+            _tapGesture = new TapGestureRecognizer {
+                Command = new Command(OnTapped)
+            };
+        }
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+            if (Parent is null)
+            {
+                GestureRecognizers.Remove(_tapGesture);
+            }
+            else
+            {
+                GestureRecognizers.Add(_tapGesture);
+            }
+        }
+
+        private void OnTapped()
+        {
+            var context = new StarredContext { 
+                Identifier = Identifier,
+                IsStarred = IsStarred
+            };
+            if (SwitchedCommand?.CanExecute(context) == true)
+            {
+                SwitchedCommand.Execute(context);
             }
         }
 
