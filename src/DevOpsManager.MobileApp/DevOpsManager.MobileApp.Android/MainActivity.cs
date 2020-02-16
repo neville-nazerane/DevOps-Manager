4d750a8c9d6f4b5778a0fe8c8a9adf02fd217607
Neville <live.neville@live.com>
2020-02-16 00:12:36 -0700

    updated icon

--- src/DevOpsManager.MobileApp/DevOpsManager.MobileApp.Android/MainActivity.cs
+++ src/DevOpsManager.MobileApp/DevOpsManager.MobileApp.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms;
 
 namespace DevOpsManager.MobileApp.Droid
 {
-    [Activity(Label = "DevOpsManager.MobileApp", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DevOpsManager.MobileApp", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -23,8 +23,8 @@ namespace DevOpsManager.MobileApp.Droid
 
             Forms.SetFlags("SwipeView_Experimental", "CarouselView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
+            FormsMaterial.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
